a97e5b1e7a689756e25f70a0264b05257d87281c
Nathaniel Collier <nathaniel.collier@aecc.com>
2018-07-07 13:36:43 -0500

    Copied in the test we have

---
+++ tests/Tests.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void CreatePopulateAndReadFromTestTable()
+        {
+            var db = new ecarkDBSQlite.Database(@"C:\users\ncollier\desktop\test.db");
+
+            db.Command(@"
+                drop table if exists test;
+                CREATE TABLE test(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    timestamp datetime  DEFAULT CURRENT_TIMESTAMP,
+                    val1 varchar(50),
+                    val2 varchar(50),
+                    val3 varchar(50)
+                );
+            ");
+
+            db.Command(@"
+                INSERT INTO test (val1,val2,val3)
+                VALUES(@v1, @v2, @v3)
+            ", new System.Collections.Generic.Dictionary<string, object>
+            {
+                {"@v1", "7" },
+                {"@v2", "Happy Birthday!" },
+                {"@v3", "8" }
+            });
+
+            var dt = db.Query(@"
+                select *
+                from test
+            ");
+
+
+            Assert.IsTrue(string.Equals(dt.Rows[0]["val2"] as string, "Happy Birthday!", StringComparison.OrdinalIgnoreCase));
+            Assert.IsTrue(string.Equals(dt.Rows[0]["val3"], "8"));
+
+        }
+    }
+}
